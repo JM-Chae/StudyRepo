c08e54839fecec4dd4b3927bf27b975bd85d1fd7
JM-Chae <jm.chae1998@gmail.com>
2024-12-13 18:08:24 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main
+  {
+    public static void main(String[] args) throws IOException
+      {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[] d = new long[n + 1];
+
+        d[0] = 1;
+        d[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+          {
+            d[i] = (d[i - 1] + d[i - 2]) % 10007;
+          }
+
+        System.out.println(d[n]);
+      }
+  }
