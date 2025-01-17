b613a1b49c1f0890708f325641e2e6a9e841aa48
JM-Chae <jm.chae1998@gmail.com>
2025-01-17 18:38:39 +0900

    [Silver I] Title: 경로 찾기, Time: 160 ms, Memory: 16716 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,48 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main
-  {
-    public static void main(String[] args) throws IOException
-      {
+class Main {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n + 1][n + 1];
 
-        for (int i = 1; i <= n; i++)
-          {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= n; j++)
-              {
-                int temp = Integer.parseInt(st.nextToken());
-                if (temp == 1) arr[i][j] = temp;
-                else arr[i][j] = 300;
-              }
-          }
+        ArrayList<Integer>[] arr = new ArrayList[n + 1];
+        int[][] ans = new int[n + 1][n + 1];
 
-        for (int i = 1; i <= n; i++)
-          {
-            for (int j = 1; j <= n; j++)
-              {
-                for (int k = 1; k <= n; k++)
-                  {
-                    arr[j][k] = Math.min(arr[j][k], arr[j][i] + arr[i][k]);
-                  }
-              }
-          }
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i] = new ArrayList<>();
+            for (int j = 1; j <= n; j++) {
+                if (st.nextToken().equals("1")) {
+                    arr[i].add(j);
+                }
+            }
+        }
 
-        for (int i = 1; i <= n; i++)
-          {
-            for (int j = 1; j <= n; j++)
-              {
-                int temp = arr[i][j];
-                if (temp == 300) System.out.print("0 ");
-                else System.out.print("1 ");
-              }
-            System.out.println();
-          }
-      }
-  }
+        for (int i = 1; i <= n; i++) {
+            Queue<Integer> q = new LinkedList<>();
+            boolean[] visited = new boolean[n + 1];
+            q.add(i);
+            while (!q.isEmpty()) {
+                int cur = q.poll();
+
+                if (!arr[cur].isEmpty()) {
+                    for (int j = 0; j < arr[cur].size(); j++) {
+                        int next = arr[cur].get(j);
+                        if (!visited[next]) {
+                            visited[next] = true;
+                            q.add(next);
+                            ans[i][next] = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append(ans[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
