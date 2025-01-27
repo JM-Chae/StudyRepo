f5ad4bd90de34e44e40e3e324556a5e91efcb378
JM-Chae <jm.chae1998@gmail.com>
2025-01-27 19:28:54 +0900

    [Silver I] Title: 나무 탈출, Time: 932 ms, Memory: 198656 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.java
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.java
@@ -2,50 +2,70 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main
-  {
+class Main {
+
+    static int n;
     static ArrayList<Integer>[] list;
+    static boolean[] visited;
     static int count = 0;
+    static int[] parent;
+    static int[] dep;
 
-    public static void main(String[] args) throws IOException
-      {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
         list = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+        parent = new int[n + 1];
+        dep = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) list[i] = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
 
-        for (int i = 1; i < n; i++)
-          {
+        for (int i = 1; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+
             list[a].add(b);
             list[b].add(a);
-          }
+        }
 
-        DFS(1, -1, 0);
+        BFS();
 
-        System.out.println((count % 2 == 1) ? "Yes" : "No");
-      }
+        System.out.println(count % 2 != 0 ? "Yes" : "No");
+    }
 
-    static void DFS(int i, int j, int d)
-      {
-        if (list[i].size() == 1 && list[i].get(0) == j)
-          {
-            count = count + d;
-            return;
-          }
+    static void BFS() {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        visited[1] = true;
+        parent[1] = 0;
 
-        for (int k = 0; k < list[i].size(); k++)
-          {
-            int c = list[i].get(k);
-            if (c == j) continue;
-            DFS(c, i, d + 1);
-          }
-      }
-  }
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            if (!list[cur].isEmpty()) {
+                if (list[cur].size() == 1 && list[cur].get(0) == parent[cur]) {
+                    count += dep[cur];
+                    continue;
+                }
+                for (int i = 0; i < list[cur].size(); i++) {
+                    if (!visited[list[cur].get(i)]) {
+                        visited[list[cur].get(i)] = true;
+                        q.add(list[cur].get(i));
+
+                        parent[list[cur].get(i)] = cur;
+                        dep[list[cur].get(i)] = dep[cur] + 1;
+                    }
+                }
+            }
+        }
+    }
+}
