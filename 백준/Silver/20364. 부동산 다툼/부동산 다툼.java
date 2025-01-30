ce7d404c3d4bf8a0c3571e33a0fbea5239c5211b
JM-Chae <jm.chae1998@gmail.com>
2025-01-30 15:38:02 +0900

    [Silver I] Title: 부동산 다툼, Time: 1592 ms, Memory: 188240 KB -BaekjoonHub

--- 백준/Silver/20364. 부동산 다툼/부동산 다툼.java
+++ 백준/Silver/20364. 부동산 다툼/부동산 다툼.java
@@ -1,59 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main
-  {
-    static ArrayList<ArrayList<Integer>> home;
+class Main {
 
-    public static void main(String[] args) throws IOException
-      {
+    static int[] arr;
+    static int[] sold;
+    static int n, q;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int q = Integer.parseInt(st.nextToken());
 
-        home = new ArrayList<>();
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1];
+        sold = new int[n + 1];
 
-        for (int i = 0; i <= n; i++)
-          {
-            home.add(new ArrayList<>());
-          }
+        for (int i = n; i >= 2; i--) {
+            arr[i] = i / 2;
+        }
 
-        for (int i = 1; i <= q; i++)
-          {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= q; i++) {
+            System.out.println(bfs(Integer.parseInt(br.readLine())));
+        }
+    }
 
-            DFS(a);
-          }
-      }
+    private static int bfs(int q) {
+        int ans = 0;
+        Queue<Integer> qq = new LinkedList<>();
+        qq.add(q);
+        while (!qq.isEmpty()) {
+            int cur = qq.poll();
+            if (sold[cur] == 1) {
+                ans = cur;
+            }
 
-    static void DFS(int n)
-      {
-        int p = n;
-        int firstDuck = 0;
-        boolean flag = false;
+            if (arr[cur] == 1) {
+                break;
+            }
 
-        while (p > 1)
-          {
-            if (!home.get(p).isEmpty())
-              {
-                flag = true;
-                firstDuck = home.get(p).get(0);
-              }
-            p = p / 2;
-          }
+            qq.add(arr[cur]);
+        }
 
-        if (!flag)
-          {
-            home.get(n).add(n);
-            System.out.println(0);
-          } else
-          {
-            System.out.println(firstDuck);
-          }
-      }
-  }
+        if (ans == 0) {
+            sold[q] = 1;
+        }
+
+        return ans;
+    }
+}
